5270fb48a884dbfc6552493cdd62238b9ccde22d
Nathaniel Inman <nate041010@gmail.com>
2015-10-27 12:58:40 -0500

    first commit.

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var path = require('path'),
+    through2 = require('through2'),
+    File = require('vinyl'),
+    jade = require('jade');
+
+module.exports = function splitJade(jsonInput){
+  return through2.obj(function(file, enc, next){
+    var json = require(jsonInput),
+        compiled,
+        fileName = { filename: file.path },
+        contents = String(file.contents);
+
+    json.forEach((entry,i)=>{
+      compiled = jadePress.compile(contents,fileName)(entry.jadeData);
+      this.push(new File({
+        base: file.path,
+        path: path.join(file.path, entry.outputName),
+        contents: new Buffer(compiled)
+      }))
+    });
+    next();
+  });
+};
