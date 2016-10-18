0c8b963235ce40e8ec0be562028d491b1bfc9f06
Nathaniel Inman <nate041010@gmail.com>
2016-10-18 17:11:29 -0500

    gulp-jade-split now supports es6 module syntax, and gracefully fails when array isnt sent.

--- index.js
+++ index.js
@@ -13,14 +13,39 @@ module.exports = function splitJade(jsonInput){
         contents = String(file.contents),
         that = this;
 
-    json.forEach(function(entry){
-      compiled = jade.compile(contents,fileName)(entry.jadeData);
-      that.push(new File({
-        base: file.path,
-        path: path.join(file.path, entry.outputName),
-        contents: new Buffer(compiled)
-      }));
-    });
+    // depending on amd or es6 importing it may present as an object
+    // with a property name instead of just an array of data
+    if(!(json instanceof Array)&&testObjectContainsArray(json)){
+      json = json[Object.keys(json)[0]];
+    } //end if
+
+    // If the sent array with content, then operate upon that
+    if(json instanceof Array&&json.length){
+      json.forEach(function(entry){
+        compiled = jade.compile(contents,fileName)(entry.jadeData);
+        that.push(new File({
+          base: file.path,
+          path: path.join(file.path, entry.outputName),
+          contents: new Buffer(compiled)
+        }));
+      });
+    }else{
+      console.log('Gulp jade split not sent an array as expected or array empty.');
+    } //end if
     next();
   });
 };
+
+function testObjectContainsArray(obj){
+  var result = true;
+
+  if(typeof obj !== 'object'){
+    result = false;
+  }else if(!Object.keys(obj).length){
+    result = false;
+  }else if(!(obj[Object.keys(obj)[0]] instanceof Array)){
+    result = false;
+  } //end if
+  return result;
+}
+
